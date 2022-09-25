9dfb04f8d61243d418ea9c6ad8f225d16b926c1d
millerlp <millerlp@gmail.com>
2022-09-24 22:17:30 -0700

    Basic data logging example
    Log data to the onboard microSD card for an Allegro Hall effect sensor and the onboard DS3231 clock's temperature sensor.

---
+++ examples/simple_logging_Hall/simple_logging_Hall.ino
@@ -0,0 +1,237 @@
+/** =========================================================================
+ *  
+ *  
+ * @file simple_logging_Hall.ino
+ * @brief A simple data logging example to read raw ADC counts from an
+ * Allegro A139x series Hall effect sensor. Modified by Luke Miller
+ *
+ * @author Sara Geleskie Damiano <sdamiano@stroudcenter.org>
+ * @copyright (c) 2017-2022 Stroud Water Research Center (SWRC)
+ *                          and the EnviroDIY Development Team
+ *            This example is published under the BSD-3 license.
+ *
+ * Build Environment: Visual Studios Code with PlatformIO
+ * Hardware Platform: EnviroDIY Mayfly Arduino Datalogger
+ * 
+ * See https://github.com/millerlp/Allegro_A139x_Hall_effect for the sensor library
+ *
+ * DISCLAIMER:
+ * THIS CODE IS PROVIDED "AS IS" - NO WARRANTY IS GIVEN.
+ * ======================================================================= */
+
+// ==========================================================================
+//  Include the libraries required for any data logger
+// ==========================================================================
+/** Start [includes] */
+// The Arduino library is needed for every Arduino program.
+#include <Arduino.h>
+
+// EnableInterrupt is used by ModularSensors for external and pin change
+// interrupts and must be explicitly included in the main program.
+#include <EnableInterrupt.h>
+
+// Include the main header for ModularSensors
+#include <ModularSensors.h>
+/** End [includes] */
+
+
+// ==========================================================================
+//  Data Logging Options
+// ==========================================================================
+/** Start [logging_options] */
+// The name of this program file
+const char* sketchName = "simple_logging_Hall.ino";
+// Logger ID, also becomes the prefix for the name of the data file on SD card
+const char* LoggerID = "Mayfly002";
+// How frequently (in minutes) to log data
+const uint8_t loggingInterval = 1;
+// Your logger's timezone.
+const int8_t timeZone = -8;  // Pacific Standard Time
+// NOTE:  Daylight savings time will not be applied!  Please use standard time!
+
+// Set the input and output pins for the logger
+// NOTE:  Use -1 for pins that do not apply
+const int32_t serialBaud = 57600;  // Baud rate for debugging
+const int8_t  greenLED   = 8;       // Pin for the green LED
+const int8_t  redLED     = 9;       // Pin for the red LED
+const int8_t  buttonPin  = 21;      // Pin for debugging mode (ie, button pin)
+const int8_t  wakePin    = 31;  // MCU interrupt/alarm pin to wake from sleep
+// Mayfly 0.x D31 = A7
+// Set the wake pin to -1 if you do not want the main processor to sleep.
+// In a SAMD system where you are using the built-in rtc, set wakePin to 1
+const int8_t sdCardPwrPin   = -1;  // MCU SD card power pin
+const int8_t sdCardSSPin    = 12;  // SD card chip select/slave select pin
+const int8_t sensorPowerPin = 22;  // MCU pin controlling main sensor power
+/** End [logging_options] */
+
+
+// ==========================================================================
+//  Using the Processor as a Sensor
+// ==========================================================================
+/** Start [processor_sensor] */
+#include <sensors/ProcessorStats.h>
+
+// Create the main processor chip "sensor" - for general metadata
+const char*    mcuBoardVersion = "v1.1";
+ProcessorStats mcuBoard(mcuBoardVersion);
+/** End [processor_sensor] */
+
+
+// ==========================================================================
+//  Maxim DS3231 RTC (Real Time Clock)
+// ==========================================================================
+/** Start [ds3231] */
+#include <sensors/MaximDS3231.h>  // Includes wrapper functions for Maxim DS3231 RTC
+
+// Create a DS3231 sensor object, using this constructor function:
+MaximDS3231 ds3231(1);
+/** End [ds3231] */
+
+// ==========================================================================
+//  Everlight ALS-PT19 Ambient Light Sensor
+// ==========================================================================
+/** Start [allegroa139x] */
+#include <Allegro_A139x.h> // https://github.com/millerlp/Allegro_A139x_Hall_effect
+
+
+// NOTE: Use -1 for any pins that don't apply or aren't being used.
+const int8_t  hallPower      = -1;    // Power pin 
+const int8_t  hallData       = A3;    // Analog 3 pin
+const uint8_t hallNumberReadings = 4;
+
+// Create a Everlight Allegro A139x hall effect sensor object
+AllegroA139x hall1(hallPower, hallData, hallNumberReadings);
+
+
+// Create ADC counts pointer for the Allegro A1395 sensor
+Variable* hall1Count = new AllegroA139x_Counts(
+    &hall1, "12345678-abcd-1234-ef00-1234567890ab");
+
+/** End [everlight_alspt19] */
+
+// ==========================================================================
+//    Settings for Additional Sensors
+// ==========================================================================
+// Additional sensors can setup here, similar to the RTC, but only if
+//   they have been supported with ModularSensors wrapper functions. See:
+//   https://github.com/EnviroDIY/ModularSensors/wiki#just-getting-started
+// Syntax for the include statement and constructor function for each sensor is
+// at
+//   https://github.com/EnviroDIY/ModularSensors/wiki#these-sensors-are-currently-supported
+//   or can be copied from the `menu_a_la_carte.ino` example
+
+
+// ==========================================================================
+//  Creating the Variable Array[s] and Filling with Variable Objects
+// ==========================================================================
+/** Start [variable_arrays] */
+Variable* variableList[] = {
+    new ProcessorStats_SampleNumber(&mcuBoard),
+    new ProcessorStats_FreeRam(&mcuBoard),
+    new ProcessorStats_Battery(&mcuBoard), 
+    new MaximDS3231_Temp(&ds3231),
+    new AllegroA139x_Counts(&hall1)
+    // Additional sensor variables can be added here, by copying the syntax
+    //   for creating the variable pointer (FORM1) from the
+    //   `menu_a_la_carte.ino` example
+    // The example code snippets in the wiki are primarily FORM2.
+};
+// Count up the number of pointers in the array
+int variableCount = sizeof(variableList) / sizeof(variableList[0]);
+
+// Create the VariableArray object
+VariableArray varArray;
+/** End [variable_arrays] */
+
+
+// ==========================================================================
+//  The Logger Object[s]
+// ==========================================================================
+/** Start [loggers] */
+// Create a logger instance
+Logger dataLogger;
+/** End [loggers] */
+
+
+// ==========================================================================
+//  Working Functions
+// ==========================================================================
+/** Start [working_functions] */
+// Flashes the LED's on the primary board
+void greenredflash(uint8_t numFlash = 4, uint8_t rate = 75) {
+    for (uint8_t i = 0; i < numFlash; i++) {
+        digitalWrite(greenLED, HIGH);
+        digitalWrite(redLED, LOW);
+        delay(rate);
+        digitalWrite(greenLED, LOW);
+        digitalWrite(redLED, HIGH);
+        delay(rate);
+    }
+    digitalWrite(redLED, LOW);
+}
+/** End [working_functions] */
+
+
+// ==========================================================================
+//  Arduino Setup Function
+// ==========================================================================
+/** Start [setup] */
+void setup() {
+    // Start the primary serial connection
+    Serial.begin(serialBaud);
+
+    // Print a start-up note to the first serial port
+    Serial.print(F("Now running "));
+    Serial.print(sketchName);
+    Serial.print(F(" on Logger "));
+    Serial.println(LoggerID);
+    Serial.println();
+
+    Serial.print(F("Using ModularSensors Library version "));
+    Serial.println(MODULAR_SENSORS_VERSION);
+
+    // Set up pins for the LED's
+    pinMode(greenLED, OUTPUT);
+    digitalWrite(greenLED, LOW);
+    pinMode(redLED, OUTPUT);
+    digitalWrite(redLED, LOW);
+    // Blink the LEDs to show the board is on and starting up
+    greenredflash();
+
+    // Set the timezones for the logger/data and the RTC
+    // Logging in the given time zone
+    Logger::setLoggerTimeZone(timeZone);
+    // It is STRONGLY RECOMMENDED that you set the RTC to be in UTC (UTC+0)
+    Logger::setRTCTimeZone(0);
+
+    // Set information pins
+    dataLogger.setLoggerPins(wakePin, sdCardSSPin, sdCardPwrPin, buttonPin,
+                             greenLED);
+
+    // Begin the variable array[s], logger[s], and publisher[s]
+    varArray.begin(variableCount, variableList);
+    dataLogger.begin(LoggerID, loggingInterval, &varArray);
+
+    // Set up the sensors
+    Serial.println(F("Setting up sensors..."));
+    varArray.setupSensors();
+
+    // Create the log file, adding the default header to it
+    // Do this last so we have the best chance of getting the time correct and
+    // all sensor names correct
+    dataLogger.createLogFile(true);  // true = write a new header
+
+    // Call the processor sleep
+    dataLogger.systemSleep();
+}
+/** End [setup] */
+
+
+// ==========================================================================
+//  Arduino Loop Function
+// ==========================================================================
+/** Start [loop] */
+void loop() {
+    dataLogger.logData();
+}
+/** End [loop] */
